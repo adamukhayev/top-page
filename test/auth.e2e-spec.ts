c9e8afb584544f54f5c17be6f39e2946516d13ff
Adam <adam.ukhayev@gmail.com>
2023-07-18 20:31:10 +0600

    изменил auth.e2e-spec.ts

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { INestApplication } from "@nestjs/common";
 import * as request from "supertest";
-import { AppModule } from "./../src/app.module";
+import { AppModule } from "../src/app.module";
 import { disconnect } from "mongoose";
 import { AuthDto } from "../src/auth/dto/auth.dto";
 
